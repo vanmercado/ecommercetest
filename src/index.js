d7ce86fb3163f9f1de96380519e99a1f2ee16966
vanmercado <vaaanmercado@gmail.com>
2025-07-30 00:15:00 +0800

    frontend ecommerce test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -23,7 +23,7 @@ import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop>
       <Provider store={store}>
         <Routes>
@@ -42,5 +42,5 @@ root.render(
       </Provider>
     </ScrollToTop>
     <Toaster />
-  </BrowserRouter>
+  </HashRouter>
 );
